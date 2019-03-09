fc7a7127f00a137b3554fbef910a9c20ae0f8708
craighillelson <craighillelson@gmail.com>
2019-03-09 16:39:27 -0500

    corrected spacing

--- lunch_roulette.py
+++ lunch_roulette.py
@@ -1,10 +1,10 @@
 """ __doc__ """
 
 EMPLOYEES = {
-  "michael@dundermifflin.com": "executive",
-  "jim@dundermifflin.com": "employee",
-  "pam@dundermifflin.com": "employee",
-  }
+    "michael@dundermifflin.com": "executive",
+    "jim@dundermifflin.com": "employee",
+    "pam@dundermifflin.com": "employee",
+    }
 
 NUMBER_OF_GUESTS = input("How many guests would you like to invite? ")
 print(NUMBER_OF_GUESTS)
