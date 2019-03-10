6d8651aadb5f8341e01ba0d3523377588d65c176
craighillelson <craighillelson@gmail.com>
2019-03-10 11:52:09 -0400

    renamed k, v variables

--- lunch_roulette.py
+++ lunch_roulette.py
@@ -12,13 +12,13 @@ EMPLOYEES_AND_EXECUTIVES = {
 EXECUTIVES = []
 EMPLOYEES = []
 
-for k, v in EMPLOYEES_AND_EXECUTIVES.items():
-    if v == "executive":
-        EXECUTIVES.append(k)
+for email, level in EMPLOYEES_AND_EXECUTIVES.items():
+    if level == "executive":
+        EXECUTIVES.append(email)
     else:
-        EMPLOYEES.append(k)
+        EMPLOYEES.append(email)
 
 NUMBER_OF_GUESTS = int(input("How many guests would you like to invite? "))
 print(NUMBER_OF_GUESTS)
 
-random_numbers_lst = random.sample(range(0, len(EMPLOYEES)), NUMBER_OF_GUESTS)
+RANDOM_NUMBERS_LST = random.sample(range(0, len(EMPLOYEES)), NUMBER_OF_GUESTS)
