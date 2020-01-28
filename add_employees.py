be2ec5d34dcda76fe4a8a1cdaadb203494eff282
craighillelson <craighillelson@gmail.com>
2020-01-28 08:20:50 -0500

    created add_employees.py

---
+++ add_employees.py
@@ -0,0 +1,64 @@
+""" __doc__ """
+
+# imports
+import csv
+import functions
+from datetime import date
+from collections import namedtuple
+
+# functions and lambdas
+RTN = lambda: '\n'
+
+# data stores
+EMPLOYEES_AND_EXECUTIVES = {}
+EMPLOYEES_TO_ADD = {}
+
+# variable
+today = date.today()
+
+# open csv and populate EMPLOYEES_AND_EXECUTIVES with its contents
+functions.open_csv('employees_and_executives.csv', EMPLOYEES_AND_EXECUTIVES)
+
+for employee, level in EMPLOYEES_AND_EXECUTIVES.items():
+    print(employee, level)
+
+print(RTN())
+
+domain = input('What is your domain name?\n')
+
+# prompt user
+while True:
+    print('Enter the employee\'s name ' + str(len(EMPLOYEES_AND_EXECUTIVES) + 1)
+          + ' (Or enter nothing to stop.):')
+    email_prefix = input()
+    if email_prefix == '':
+        break
+    email = email_prefix + '@' + domain
+    exec = input('Is the employee an executive (y/n)?\n')
+    if exec == 'y':
+        level = 'executive'
+    else:
+        level = 'employee'
+    EMPLOYEES_AND_EXECUTIVES[email] = level
+    EMPLOYEES_TO_ADD[email] = level
+
+# populate dictionary with user input
+EMPLOYEES_AND_EXECUTIVES[email] = level
+
+# write user input to 'employees_and_executives.csv'
+HEADERS = 'employee','level'
+
+with open('employees_and_executives.csv', 'w') as out_file:
+    out_csv = csv.writer(out_file)
+    out_csv.writerow(HEADERS)
+    for email, level in EMPLOYEES_AND_EXECUTIVES.items():
+        keys_values = (email, level)
+        out_csv.writerow(keys_values)
+
+# update user
+for email, level in EMPLOYEES_TO_ADD.items():
+    print('employee added')
+    print(f'key: {email}')
+    print(f'value: {level}')
+
+print(RTN())
