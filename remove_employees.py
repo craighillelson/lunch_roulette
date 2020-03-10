07b454b360521222cbcfa3a169417af4455af247
Craig Hillelson <craighillelson@gmail.com>
2020-03-10 13:46:54 -0400

    update remove_employees.py
    edited function calls

--- remove_employees.py
+++ remove_employees.py
@@ -8,7 +8,7 @@ EMPLOYEES_AND_EXECUTIVES = {}
 EMPLOYEES_AND_EXECUTIVES_UPDATED = {}
 
 # populate dictonary and provide options to user
-functions.open_csv('employees_and_executives.csv', EMPLOYEES_AND_EXECUTIVES)
+EMPLOYEES_AND_EXECUTIVES = functions.open_csv('employees_and_executives.csv')
 
 for i, (email, level) in enumerate(EMPLOYEES_AND_EXECUTIVES.items(), 1):
     print(f'{i} {email} {level}')
@@ -20,7 +20,6 @@ del_emp = input('select an employee to remove from list\n')
 
 for i, (email, level) in enumerate(EMPLOYEES_AND_EXECUTIVES.items(), 1):
     if del_emp != email:
-        print(f'{i} {email} {level}')
         EMPLOYEES_AND_EXECUTIVES_UPDATED[email] = level
     else:
         pass
@@ -29,8 +28,8 @@ print(functions.RTN())
 
 # update user
 print('updated list')
-for email, level in EMPLOYEES_AND_EXECUTIVES_UPDATED.items():
-    print(email, level)
+for i, (email, level) in enumerate(EMPLOYEES_AND_EXECUTIVES_UPDATED.items(), 1):
+    print(i, email, level)
 
 functions.write_dct_to_csv(EMPLOYEES_AND_EXECUTIVES_UPDATED)
 
