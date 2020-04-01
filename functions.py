890988b27ecd1f26f06f19e823f1db1f8e87646a
craighillelson <craighillelson@gmail.com>
2020-04-01 08:04:19 -0400

    update functions.py
    added output_not_selected

--- functions.py
+++ functions.py
@@ -1,17 +1,21 @@
 """ __doc__ """
 
-from collections import namedtuple
 import csv
+import pyinputplus as pyip
+from collections import namedtuple
+
 
 RTN = lambda: '\n'
 
 GUESTS = []
 
 def print_return():
+    """Print a return."""
     print('\n')
 
+
 def open_csv(file):
-    """ open csv and populate a dictionary with its contents """
+    """ open a csv and populate a dictionary with its contents """
     DCT = {}
 
     with open(file) as csv_file:
@@ -51,13 +55,25 @@ def write_list_to_csv(output_csv, output_lst):
             out_csv.writerow([employee_email])
 
 
-def output_selections(a, b):
-    """ print employees in list """
+def output_selected(a, b):
+    """Print host and selected employees."""
+    print('Host')
     print(a)
+    print_return()
+    print('Guests')
     for employee in b:
         print(employee)
 
-    print(RTN())
+    print_return()
+
+
+def output_not_selected(a):
+    """Print employees not selected."""
+    print('The following employees were not selected:')
+    for employee in a:
+        print(employee)
+
+    print_return()
 
 
 def update_user(results):
