d287ebdeb3bc0fb0f5738f5fba50474b667aab28
Craig Hillelson <craighillelson@gmail.com>
2020-05-31 20:48:01 -0400

    upda

--- add_employees.py
+++ add_employees.py
@@ -1,19 +1,17 @@
-""" __doc__ """
+"""
+Give user the ability to add employees to the 'employees_and_executives.csv.'
+"""
 
-# imports
 import csv
 import functions
 from datetime import date
 from collections import namedtuple
 
-# data stores
 EMPLOYEES_AND_EXECUTIVES = {}
 EMPLOYEES_TO_ADD = {}
 
-# variable
 today = date.today()
 
-# open csv and populate EMPLOYEES_AND_EXECUTIVES with its contents
 EMPLOYEES_AND_EXECUTIVES = functions.open_csv('employees_and_executives.csv')
 
 for employee, level in EMPLOYEES_AND_EXECUTIVES.items():
@@ -23,7 +21,6 @@ print(functions.RTN())
 
 domain = input('What is your domain name?\n')
 
-# prompt user
 while True:
     print('Enter the employee\'s name (or \'return\' to stop.):')
     email_prefix = input()
@@ -38,12 +35,10 @@ while True:
     EMPLOYEES_AND_EXECUTIVES[email] = level
     EMPLOYEES_TO_ADD[email] = level
 
-# populate dictionary with user input
 EMPLOYEES_AND_EXECUTIVES[email] = level
 
 functions.write_dct_to_csv(EMPLOYEES_AND_EXECUTIVES)
 
-# update user
 for email, level in EMPLOYEES_TO_ADD.items():
     print('employee added')
     print(f'key: {email}')
