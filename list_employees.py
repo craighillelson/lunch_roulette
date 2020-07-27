6e916004ce65af8a5350f504ce7e694624a48d29
Craig Hillelson <craighillelson@gmail.com>
2020-07-27 17:52:44 -0400

    update list_employees.py
    moved functions to functions.py

--- list_employees.py
+++ list_employees.py
@@ -2,15 +2,5 @@
 
 import functions
 
-
-def list_emps(a):
-    """Output employees and executives."""
-    functions.print_return()
-    print('employees and executives')
-    for employee, level in sorted(a.items()):
-        print(f'{employee}, {level}')
-    functions.print_return()
-
-
-EMPLOYEES_AND_EXECUTIVES = functions.open_csv('employees_and_executives.csv')
-list_emps(EMPLOYEES_AND_EXECUTIVES)
+employees_and_executives = functions.open_csv_pop_dct_namedtuple()
+functions.output_employees_and_executives(employees_and_executives)
