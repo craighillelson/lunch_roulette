6debfa1e333da2d2407f7a8e87c635d46297f940
Craig Hillelson <craighillelson@gmail.com>
2020-07-27 17:51:41 -0400

    update functions.py
    refactored

--- functions.py
+++ functions.py
@@ -1,87 +1,104 @@
-"""Lunch roulette functions."""
+"""Functions."""
 
-import csv
 import pyinputplus as pyip
-from collections import namedtuple
+
+EMPLOYEES_CSV = 'csvs/employees_and_executives.csv'
 
 
-RTN = lambda: '\n'
+def not_a_weekend(user_specified_date):
+    """Determine if the date entered is not a weekday."""
+    import datetime
 
-GUESTS = []
+    day_index = user_specified_date.weekday()
+    return day_index < 5
 
 
-def print_return():
-    """Print a return."""
+def open_csv_pop_dct_namedtuple():
+    """Open a csv and populate a dictionary."""
+    import csv
+    from collections import namedtuple
+
+    dct = {}
+
+    with open(EMPLOYEES_CSV) as f:
+        f_csv = csv.reader(f)
+        headings = next(f_csv)
+        Row = namedtuple('Row', headings)
+        for r in f_csv:
+            row = Row(*r)
+            dct[row.email] = row.level
+
+    return dct
+
+
+def output_employees_not_selected(lst1, lst2):
+    """Print a list of employees not selected."""
+    print('\nemployees not selected')
+    for email in lst1:
+        if email not in lst2:
+            print(email)
+        else:
+            pass
+
+
+def output_employees_and_executives(dct):
+    """Print a list of all employees and executives."""
     print('\n')
+    print('email,level')
+    for email, level in dct.items():
+        print(email, level)
 
 
-def open_csv(file):
-    """Open a csv and populate a dictionary with its contents."""
-    DCT = {}
+def prompt_user_for_number_guests_to_invite():
+    """Prompt user for the number of guests they'd like to invite."""
 
-    with open(file) as csv_file:
-        F_CSV = csv.reader(csv_file)
-        COLUMN_HEADINGS = next(F_CSV)
-        CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
-        for rows in F_CSV:
-            row = CSV_ROW(*rows)
-            DCT[row.email] = row.level
-
-    return DCT
+    return pyip.inputInt('\nHow many guests would you like to invite?\n> ')
 
 
-def append_list(a, b, c, d):
-    """Add selections to a list."""
-    for a in b:
-        c.append(d[a])
+def remove_selected_employees(dct2, lst):
+    """
+    Remove selected employees from the master list of employees and executives.
+    """
+    dct1 = {}
+    for email, level in dct2.items():
+        if email not in lst:
+            dct1[email] = level
+        else:
+            pass
+
+    return dct1
 
 
-def write_dct_to_csv(a):
+def select_an_executive(lst):
+    """Prompt user to select an executive."""
+    print('\nPlease select an executive.')
+    for num, executive in enumerate(lst, 1):
+        print(num, executive)
+    return pyip.inputMenu(lst, prompt='> ', blank=True, numbered=True)
+
+
+def write_employees_to_csv(file, DCT):
     """Write dictionary to csv."""
-    HEADERS = 'email','level'
-    with open('employees_and_executives.csv', 'w') as out_file:
+    import csv
+
+    with open(file, 'w') as out_file:
         out_csv = csv.writer(out_file)
-        out_csv.writerow(HEADERS)
-        for email, level in a.items():
+        out_csv.writerow(['email','level'])
+        for email, level in DCT.items():
             keys_values = (email, level)
             out_csv.writerow(keys_values)
 
+    print(f'"{file}" exported successfully')
 
-def write_list_to_csv(output_csv, output_lst):
-    """Write results to csv, print same, and print a return for readability."""
-    with open(output_csv, 'w') as out_file:
+
+def write_lst_to_csv(file, lst):
+    """Write list to csv """
+    import csv
+
+    with open(file, 'w') as out_file:
         out_csv = csv.writer(out_file)
-        out_csv.writerow(['email'])
-        for employee_email in output_lst:
-            out_csv.writerow([employee_email])
+        out_csv.writerow(['guests'])
+        for i in lst: # rename i to make it meaningful
+            out_csv.writerow([i])
 
-
-def output_selected(a, b):
-    """Print host and selected employees."""
-    print('Host')
-    print(a)
-    print_return()
-    print('Guests')
-    for employee in b:
-        print(employee)
-
-    print_return()
-
-
-def output_not_selected(a):
-    """Print employees not selected."""
-    print('The following employees were not selected:')
-    for employee in a:
-        print(employee)
-
-    print_return()
-
-
-def update_user(results):
-    """Update user."""
-    print(f'{results} exported successfully')
-
-
-def open_file(a):
-    """Open python script in response to user selection."""
-    exec(open(a).read())
+        print(f'\n"{file}" exported successfully')
