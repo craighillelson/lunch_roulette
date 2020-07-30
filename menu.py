63850225ff80f757f400cc4e1a478677ef72aea7
craighillelson <craighillelson@gmail.com>
2020-07-30 07:26:30 -0400

    update menu.py
    edited doc string, simplified code

--- menu.py
+++ menu.py
@@ -1,27 +1,23 @@
-"""Provide a list of options to the user."""
+"""Provide user with a list of options and prompt them to make a selection."""
 
+# import pyinputplus as pyip
 import functions
-import pyinputplus as pyip
 
-options = {
-    1: 'add_employees.py',
-    2: 'list_employees.py',
-    3: 'lunch_roulette.py',
-    4: 'remove_employees.py',
-}
-
-def switch_case(argument):
-    """Switch case statement."""
-    options
-    return options.get(argument, 'nothing')
+choices = [
+    'add_employees.py',
+    'list_employees.py',
+    'lunch_roulette.py',
+    'remove_employees.py',
+    'quit',
+    ]
 
 while True:
-    print('\nMake a selection or press enter to quit')
-    for num, option in options.items():
-        print(num, option)
-    usr_choice = pyip.inputInt('> ', min=1, max=len(options), blank=True)
-    if usr_choice != '':
-        exec(open(switch_case(usr_choice)).read())
+    print('\nPlease select from the options below.')
+    for num, choice in enumerate(choices, 1):
+        print(num, choice)
+    selection = functions.pyip.inputMenu(choices, prompt='> ', blank=True,
+                                         numbered=True)
+    if selection != 'quit':
+        exec(open(selection).read())
     else:
-        print('\nSession complete.\n')
         break
