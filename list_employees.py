5185319df2a330be64b0786409aaa7634db1632d
craighillelson <craighillelson@gmail.com>
2020-07-30 18:47:42 -0400

    update list_employees.py
    added logic to check to alert user if there are no employee records in the database

--- list_employees.py
+++ list_employees.py
@@ -4,4 +4,7 @@ from functions import (open_csv_pop_dct_namedtuple,
                        output_employees_and_executives)
 
 employees_and_executives = open_csv_pop_dct_namedtuple()
-output_employees_and_executives(employees_and_executives)
+if employees_and_executives:
+    output_employees_and_executives(employees_and_executives)
+else:
+    print('\nno employees in database')
