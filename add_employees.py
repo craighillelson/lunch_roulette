993972ad22d94f18e26040247fc7754170d2296d
craighillelson <craighillelson@gmail.com>
2020-07-30 07:24:26 -0400

    update add_employees.py
    incorporated LEVEL_MAP dct

--- add_employees.py
+++ add_employees.py
@@ -5,23 +5,24 @@ import pyinputplus as pyip
 import random
 
 employees_and_executives = functions.open_csv_pop_dct_namedtuple()
-
 emails_to_add = {}
+
+LEVEL_MAP = {
+    'yes': 'executive',
+    'no': 'employee',
+    }
+
 while True:
     print('\nenter an eamil address or enter nothing to quit')
     email = pyip.inputEmail('> ', blank=True)
-    if email != '':
-        if email not in employees_and_executives.keys():
-            level = pyip.inputYesNo('is this employee an executive?\n> ')
-            if level != 'yes':
-                level = 'employee'
-            else:
-                level = 'executive'
-            emails_to_add[email] = level
-        else:
-            print('email already in csv')
-    else:
+    if email == '':
         break
+
+    if email not in employees_and_executives.keys():
+        level = functions.pyip.inputYesNo('is this employee an executive?\n> ')
+        emails_to_add[email] = LEVEL_MAP[level]
+    else:
+        print('email already in csv')
 
 employees_and_executives_updated = {**employees_and_executives, **emails_to_add}
 print('\n')
