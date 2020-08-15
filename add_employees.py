6e45fe90bd85f41d58c0764221e0209646d5c1e1
craighillelson <craighillelson@gmail.com>
2020-08-15 08:08:14 -0400

    update add_employees.py
    edited import statement so that only the functions called are imported

--- add_employees.py
+++ add_employees.py
@@ -1,54 +1,48 @@
 """Add employees to csv."""
 
-import functions
+from functions import (open_csv_pop_dct_namedtuple,
+                       prompt_user_for_email_prefix,
+                       concat_prefix_and_domain,
+                       add_email_if_not_in_dct,
+                       write_employees_to_csv,
+                       EMPLOYEES_CSV)
+import pyinputplus as pyip
 import random
 
-employees_and_executives = functions.open_csv_pop_dct_namedtuple()
+employees_and_executives = open_csv_pop_dct_namedtuple()
 
 LEVEL_MAP = {
-    'yes': 'executive',
-    'no': 'employee',
+    "yes": "executive",
+    "no": "employee",
     }
 
 email_addresses = list(employees_and_executives.keys())
 domain = email_addresses[0].split('@')[1]
-domain_answer = functions.pyip.inputYesNo(f'\nIs {domain} the domain name for '
-                                          'users you\'d like to add?\n> ')
+domain_answer = pyip.inputYesNo(f"\nIs {domain} the domain name for users "
+                                f"you'd like to add?\n> ")
 emails_to_add = {}
 
-if domain_answer == 'yes':
-    while True:
-        email_prefix = functions.prompt_user_for_email_prefix()
-        if email_prefix == '':
-            break
-
-        email_address_to_add = functions.concat_prefix_and_domain(email_prefix,
-                                                                  domain)
-        level = functions.pyip.inputYesNo('is this employee an executive?\n> ')
-        functions.add_email_if_not_in_dct(email_address_to_add,
-                                          employees_and_executives,
-                                          emails_to_add,
-                                          LEVEL_MAP, level)
+if domain_answer != "yes":
+    domain = pyip.inputStr("Please enter the domain name associated with the "
+                           "users you'd like to add\n> ")
 else:
-    domain = pyip.inputStr('Please enter the domain name associated with '
-                           'the users you\'d like to add\n> ')
-    while True:
-        email_prefix = functions.prompt_user_for_email_prefix()
-        if email_prefix == '':
-            break
+    pass
 
-        level = functions.pyip.inputYesNo('is this employee an executive?\n> ')
-        email_address_to_add = functions.concat_prefix_and_domain(email_prefix,
-                                                                  domain)
-        functions.add_email_if_not_in_dct(email_address_to_add,
-                                          employees_and_executives,
-                                          emails_to_add,
-                                          LEVEL_MAP, level)
+while True:
+    email_prefix = prompt_user_for_email_prefix()
+    if email_prefix == "":
+        break
+
+    email_address_to_add = concat_prefix_and_domain(email_prefix, domain)
+    level = pyip.inputYesNo("is this employee an executive?\n> ")
+    add_email_if_not_in_dct(email_address_to_add,
+                            employees_and_executives,
+                            emails_to_add,
+                            LEVEL_MAP, level)
 
 employees_and_executives_updated = {**employees_and_executives, **emails_to_add}
-print('\n')
+print("\n")
 for email, level in  employees_and_executives_updated.items():
     print(email, level)
 
-functions.write_employees_to_csv(functions.EMPLOYEES_CSV,
-                                 employees_and_executives_updated)
+write_employees_to_csv(EMPLOYEES_CSV, employees_and_executives_updated)
