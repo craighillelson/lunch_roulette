665a4a49454b39365faaa24cc46a200ae66828ee
craighillelson <craighillelson@gmail.com>
2020-08-19 07:10:17 -0400

    update list_employees.py
    renamed variables to conform to PEP8 standards

--- list_employees.py
+++ list_employees.py
@@ -3,8 +3,8 @@
 from functions import (open_csv_pop_dct_namedtuple,
                        output_employees_and_executives)
 
-employees_and_executives = open_csv_pop_dct_namedtuple()
-if employees_and_executives:
-    output_employees_and_executives(employees_and_executives)
+EMPLOYEES_AND_EXECUTIVES = open_csv_pop_dct_namedtuple()
+if EMPLOYEES_AND_EXECUTIVES:
+    output_employees_and_executives(EMPLOYEES_AND_EXECUTIVES)
 else:
     print('\nno employees in database')
