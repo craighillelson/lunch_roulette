d3c6bc0f589c89db51fcb5b7a8edb798af40be3e
Craig Hillelson <craighillelson@gmail.com>
2020-10-03 13:00:24 -0400

    update list_employees.py
    changed string quoting to be consistent

--- list_employees.py
+++ list_employees.py
@@ -7,4 +7,4 @@ EMPLOYEES_AND_EXECUTIVES = open_csv_pop_dct_namedtuple()
 if EMPLOYEES_AND_EXECUTIVES:
     output_employees_and_executives(EMPLOYEES_AND_EXECUTIVES)
 else:
-    print('\nno employees in database')
+    print("\nno employees in database")
