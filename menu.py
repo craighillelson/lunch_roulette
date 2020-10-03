629be5f2f388042c76f1d0379755e41cdb7f1f23
Craig Hillelson <craighillelson@gmail.com>
2020-10-03 13:06:51 -0400

    update menu.py
    - updated string quoting to make it consistent with other files
    - moved closing bracket to align with start of the list definition

--- menu.py
+++ menu.py
@@ -1,23 +1,22 @@
 """Provide user with a list of options and prompt them to make a selection."""
 
-# import pyinputplus as pyip
 import functions
 
 choices = [
-    'add_employees.py',
-    'list_employees.py',
-    'lunch_roulette.py',
-    'remove_employees.py',
-    'quit',
-    ]
+    "add_employees.py",
+    "list_employees.py",
+    "lunch_roulette.py",
+    "remove_employees.py",
+    "quit",
+]
 
 while True:
-    print('\nPlease select from the options below.')
+    print("\nPlease select from the options below.")
     for num, choice in enumerate(choices, 1):
         print(num, choice)
-    selection = functions.pyip.inputMenu(choices, prompt='> ', blank=True,
+    selection = functions.pyip.inputMenu(choices, prompt="> ", blank=True,
                                          numbered=True)
-    if selection != 'quit':
+    if selection != "quit":
         exec(open(selection).read())
     else:
         break
