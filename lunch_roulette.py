0309a923e458d4082f1957acf976bf9db54c0097
Craig Hillelson <craighillelson@gmail.com>
2020-10-03 13:03:32 -0400

    update lunch_roulette.py
    edited string quoting to make it consistent

--- lunch_roulette.py
+++ lunch_roulette.py
@@ -9,13 +9,13 @@ from datetime import date
 today = date.today()
 
 while True:
-    lunch_date = functions.pyip.inputDate(prompt='\nPlease enter a date for '
-                                          'lunch in \'YYYY-MM-DD\' format\n> ',
-                                          formats=['%Y-%m-%d'])
+    lunch_date = functions.pyip.inputDate(prompt="\nPlease enter a date for "
+                                          "lunch in 'YYYY-MM-DD' format\n> ",
+                                          formats=["%Y-%m-%d"])
     if lunch_date <= today:
-        print('Please select a date in the future')
+        print("Please select a date in the future")
     elif functions.not_a_weekend(lunch_date) == False:
-        print('Please select a date that doesn\'t fall on a weekend.')
+        print("Please select a date that doesn't fall on a weekend.")
     else:
         break
 
@@ -24,7 +24,7 @@ employeess_and_executives = functions.open_csv_pop_dct_namedtuple()
 employees = []
 executives = []
 for email, level in employeess_and_executives.items():
-    if level != 'executive':
+    if level != "executive":
         employees.append(email)
     else:
         executives.append(email)
@@ -34,8 +34,8 @@ user_selected_executive = functions.select_an_executive(executives)
 number_of_guests = functions.prompt_user_for_number_guests_to_invite()
 
 random_guests = random.sample(range(0, len(employees)), number_of_guests)
-print(f'\nsuggested lunch guests to join {user_selected_executive} on '
-      f'{lunch_date}')
+print(f"\nsuggested lunch guests to join {user_selected_executive} on "
+      f"{lunch_date}")
 
 guests = []
 
@@ -47,5 +47,5 @@ for num, email in enumerate(guests, 1):
 
 functions.output_employees_not_selected(employees, guests)
 
-guest_list = str(lunch_date) + '_' + str(user_selected_executive) + '.csv'
+guest_list = str(lunch_date) + "_" + str(user_selected_executive) + ".csv"
 functions.write_lst_to_csv(guest_list, guests)
